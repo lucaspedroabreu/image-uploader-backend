fd5a6efaaf48a688ece4103884fd83a136b08b1e
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2021-01-28 14:31:01 -0300

    added connection to local docker mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
+
+// Database setup
+mongoose.connect('mongodb://localhost:27017/imagesDatabase', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(express.json()) // allows express to receive requests with json content
 app.use(express.urlencoded({ extended: true })) // middleware that allow express to deal with encoded URLs
