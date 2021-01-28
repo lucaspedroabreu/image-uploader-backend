e3503f392452e9799eef7bec87b4638f779d3ebb
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2021-01-28 19:19:04 -0300

    added multer-s3 to serve files to AWS S3 servers

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,17 +1,8 @@
-// multer configuration file
-
-/**
- * LEARNING COMMENT
- * multer is a middleware and as such
- * it will intercept route calls with its method.
- * With separation of concern in mind,
- * we inject its parameters in another file (this)
- * and then require this file on routes file
- */
-
 const multer = require('multer')
+const multerS3 = require('multer-s3') // multer-s3 is an extension to multer that serves files to AWS S3
 const path = require('path') // guarantees correct path between different OS
 const crypto = require('crypto') // lib used to hash files, passwords, et cetera
+
 
 const localPath = path.resolve(__dirname, '..', '..', 'tmp', 'uploads')
 
@@ -48,3 +39,17 @@ module.exports = {
 }
 
 
+// multer configuration file
+
+/**
+ * LEARNING COMMENT
+ * multer is a middleware and as such
+ * it will intercept route calls with its method.
+ * With separation of concern in mind,
+ * we inject its parameters in another file (this)
+ * and then require this file on routes file
+ * 
+ * multer-s3 is a multer extension to serve files to AWS S3 servers
+ * 
+ * AWS SDK is the library that allows to connect to AWS Servers
+ */
