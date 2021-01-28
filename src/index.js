a0c61bbc169f282c839e05311b1db5325829fe70
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2021-01-27 21:33:07 -0300

    added app uses (json, urlencoded, morgan)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 const port = 3000
 
+app.use(express.json()) // allows express to receive requests with json content
+app.use(express.urlencoded({ extended: true })) // middleware that allow express to deal with encoded URLs
+app.use(morgan('dev')) // middleware that logs requests and errors
+
 app.use(require('./routes'))
+
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`)
 })
