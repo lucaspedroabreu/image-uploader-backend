988ace0766879b22fa0387520680cb1d0eb925c9
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2021-01-31 21:22:08 -0300

    added cors to authorize cross requests

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 const port = 3000
@@ -11,6 +12,7 @@ const port = 3000
 // Database setup
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(cors()) // filters domains that can access the api (blank = unrestricted access, no cors = anyone blocked)
 app.use(express.json()) // allows express to receive requests with json content
 app.use(express.urlencoded({ extended: true })) // middleware that allow express to deal with encoded URLs
 app.use(morgan('dev')) // middleware that logs requests and errors
